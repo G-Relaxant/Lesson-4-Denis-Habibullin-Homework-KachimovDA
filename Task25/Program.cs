47ff80a16a4a4d952744b1e51436c8df9f96c0dd
geekbrains <info@geekbrains.ru>
2022-06-22 17:52:56 +0400

    task25

---
+++ Task25/Program.cs
@@ -0,0 +1,16 @@
+﻿double num = Input("Введите число 1: ");
+double num2 = Input("Введите число 2: ");
+double product = num;
+for(int i = 1; i < num2; i++)
+{
+    product = product * num;
+}
+Console.Write("Число 1 в степени числа 2: " + product);
+
+
+
+double Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToDouble(Console.ReadLine());
+}
